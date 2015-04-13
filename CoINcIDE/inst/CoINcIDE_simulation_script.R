7a2ac67d0ad6c8949e30d8e35672bbe9a8f52a2a
kplaney <kplaney@isis-genome.stanford.edu>
2015-04-13 12:01:28 -0700

    starting lung simulation scripts as final debug before ovarian analysis

--- CoINcIDE/inst/CoINcIDE_simulation_script.R
+++ CoINcIDE/inst/CoINcIDE_simulation_script.R
@@ -1,36 +1,48 @@
 
+source("/home/kplaney/gitRepos/CoINcIDE/coincide/CoINcIDE/R/CoINcIDE_simulation.R")
 #create two simulated high quality datasets with a small amount of noise
-lungSimData <- createLungSimDatasets(numSimDatasets=2, stddevNoise=.1)
+lungSimData <- createLungSimDatasets(numSimDatasets=1, stddevNoise=.1)
 dim(lungSimData$dataMatrixList[[1]])
 
-clustFeatures <- lungSimData$clustFeaturesList[[1]]
+dataMatrixList <- lungSimData$dataMatrixList
+clustFeaturesList <- lungSimData$clustFeaturesList
 
-dataset <- dataMatrix[rownames(lungSimData$dataMatrixList[[1]]) %in% clustFeatures, , drop=FALSE]
-##test my gap statistic
- source('~/Dropbox/Gevaert_lab/code_RNAseq_pipeline/RP_gapTest.R')
- test <- gapTest(dataset,k=c(1:40),nstart=25,iter.max=10,clusterMethod=c("kmeans"),
-                     hclustAlgorithm="complete",distMatrix=NULL,stopAtBestK=TRUE,
-                    kmeansAlgorithm="Hartigan-Wong",clusterColumns=TRUE,numSimulations=10,refDistMethod=c("svd"))
-
- clustF <- function(x,k){
-    #k-means clusters the ROWs
-    #it turns out that R will recognize the upper-level function input value in this function.
-    #transpose BEFORE feed in here; I found it to return odd results if
-    #I feed in t(x) in the kmeans function that is feed into clusGap
-    kmeans(x, centers=k,iter.max=10,nstart=25,algorithm="Hartigan-Wong");
-  
-  }
-  
-  #k-means clusters the ROWs - so transpose dataset
-  gapTestOutput <- clusGap(t(dataset), FUNcluster=clustF,K.max=maxNumClusters, B = numSims, verbose = interactive());
-
-#best K?  4 still - i.e. the methods match up. might as well use 'cluster' package and not your own code.
-maxSE(f=gapTestOut$Tab[,"gap"], SE.f=gapTestOut$Tab[,"SE.sim"],
-       method = c("globalSEmax"))
+#let's try the four ways I've coded up clustering, selecting K.
+source("/home/kplaney/gitRepos/CoINcIDE/coincide/CoINcIDE/R/CoINcIDE_cluster.R")
+source("/home/kplaney/gitRepos/CoINcIDE/coincide/CoINcIDE/R/CoINcIDE_computeEdges.R")
+clusterOut_hclust <- clusterMatrixListHclustGap(dataMatrixList=dataMatrixList,clustFeaturesList=clustFeaturesList,maxNumClusters=30,algorithm="ward.D",
+                                                distMethod=c("euclidean"),outputFile="/home/kplaney/ovarian_analysis//cluster_hclustGap_output.txt",
+                                                corUse=c("everything"),numSims=100)
 
 
-#let's try hclust gap
-lung_hclust <- clusterMatrixListHclustGap(dataMatrixList,clustFeaturesList,maxNumClusters=10,algorithm="ward.D",
-                              distMethod=c("euclidean"),outputFile="~/Desktop/cluster_hclustGap_output.txt",numSims=100)
+message(paste0("Best K as determined by hclust and gap test is: ", unlist(clusterOut_hclust$bestK)))
+clusterOut_kmeans <- clustMatrixListKmeansGap(dataMatrixList,clustFeaturesList,maxNumClusters=30,iter.max=30,nstart=25,numSims=100,algorithm="Hartigan-Wong",outputFile="/home/kplaney/ovarian_analysis//cluster_kmeansGap_output.txt",
+                                              bestK=c("firstMatch"))
 
-lung_hclust$bestK
+message(paste0("Best K as determined by kmeans and gap test is: ", unlist(clusterOut_kmeans$bestK)))
+
+CoINcIDE_getAdjMatrices(dataMatrixList,clustSampleIndexList,clustFeatureIndexList,
+                                    edgeMethod=c("distCor","spearman","pearson","kendall","Euclidean","cosine",
+                                                 "Manhattan","Minkowski","Mahalanobis"),numParallelCores=1,minTrueSimilThresh=-Inf,maxTrueSimilThresh=Inf,
+                                    sigMethod=c("meanMatrix","centroid"),maxNullFractSize=.1,numSims=100,includeRefClustInNull=TRUE,                        
+                                    outputFile="./CoINcIDE_messages.txt",fractFeatIntersectThresh=0,numFeatIntersectThresh=0 ,clustSizeThresh=0, clustSizeFractThresh=0)
+
+
+
+clusterOut_kmeansConsensus <- consensusClusterMatrixList(dataMatrixList,clustFeaturesList,maxNumClusters = 30, numSims=100, pItem=0.8, pFeature=1, clusterAlg=c("km"),
+                                                         innerLinkage="average", finalLinkage_hclust="average",
+                                                         corUse=c("everything"),
+                                                         distMethod=c("euclidean"),
+                                                         minClustConsensus=.7,bestKmethod=c("highestMinConsensus"),
+                                                         outputFile="/home/kplaney/ovarian_analysis/consensus_kmeans.txt")
+
+message(paste0("Best K as determined by kmeans and consensus clustering is: ", unlist(clusterOut_kmeansConsensus$bestK)))
+
+clusterOut_hclustConsensus <- consensusClusterMatrixList(dataMatrixList,clustFeaturesList,maxNumClusters = 30, numSims=100, pItem=0.8, pFeature=1, clusterAlg=c("hc"),
+                                                         innerLinkage="average", finalLinkage_hclust="average",
+                                                         corUse=c("everything"),
+                                                         distMethod=c("euclidean"),
+                                                         minClustConsensus=.7,bestKmethod=c("highestMinConsensus"),
+                                                         outputFile="/home/kplaney/ovarian_analysis/consensus_hclust.txt")
+
+
